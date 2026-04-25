dbd2c341ee288c4b85fe3529e27775ca93b35d38
Seth Curry <seth@scurry.io>
2026-04-25 16:29:39 -0400

    Adding config module back

---
+++ src/legba/config.clj
@@ -0,0 +1,28 @@
+(ns legba.config
+  (:require [clojure.java.io :as io]
+            [legba.json :refer [<-json]]
+            [schema.core :as s]))
+
+(s/defschema Config
+  "A configuration for the Legba server."
+  {:database {:hostname s/Str
+              :port s/Int
+              :username s/Str
+              :password s/Str
+              :database s/Str}})
+
+(defn config-file
+  "Resolves ~/.config/legba/config.json using the process user.home."
+  []
+  (io/file (System/getProperty "user.home") ".config" "legba" "config.json"))
+
+(defn read-config
+  "Reads JSON from `path` (File, string path, or URI) into a Clojure map with
+  keyword keys. With no args, reads the default ~/.config/legba/config.json"
+  ([] (read-config (config-file)))
+  ([path]
+   (<-json (io/file path) :schema Config)))
+
+
+(defonce loaded-config
+  (delay (read-config)))
