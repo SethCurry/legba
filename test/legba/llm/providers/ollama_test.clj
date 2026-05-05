c15607669edcc3d8623f9cfdb807024599b70d5c
Seth Curry <scurry@atlassian.com>
2026-05-05 11:15:02 -0400

    Adding unit tests for ollama client

--- test/legba/llm/providers/ollama_test.clj
+++ test/legba/llm/providers/ollama_test.clj
@@ -5,4 +5,14 @@
 
 (deftest ClientOptions-test
   (testing "empty ClientOptions passes"
-    (is (true? (malli/validate ollama/ClientOptions {})))))
+    (is (true? (malli/validate ollama/ClientOptions {}))))
+  (testing "only base-url passes"
+    (is (true? (malli/validate ollama/ClientOptions {:base-url "http://localhost:11434"}))))
+  (testing "only token passes"
+    (is (true? (malli/validate ollama/ClientOptions {:token "abc123"}))))
+  (testing "base-url and token passes"
+    (is (true? (malli/validate ollama/ClientOptions {:base-url "http://localhost:11434" :token "abc123"}))))
+  (testing "invalid base-url fails"
+    (is (false? (malli/validate ollama/ClientOptions {:base-url 1234}))))
+  (testing "invalid token fails"
+    (is (false? (malli/validate ollama/ClientOptions {:token 1234})))))
