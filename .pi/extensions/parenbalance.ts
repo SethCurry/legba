e99a50f5e7e503abf69aff83082d97f6407cf8d0
Seth Curry <seth@scurry.io>
2026-05-10 11:22:17 -0400

    Adding brepl skill

--- .pi/extensions/parenbalance.ts
+++ .pi/extensions/parenbalance.ts
@@ -5,11 +5,12 @@ import {
 import child_process from "child_process";
 
 export default function (pi: ExtensionAPI) {
-  pi.on("tool_execution_end", async (event, ctx) => {
+  pi.on("tool_result", async (event, ctx) => {
     if (
       isToolCallEventType("write", event) ||
       isToolCallEventType("edit", event)
     ) {
+      ctx.ui.notify(JSON.stringify(event));
       if (event.input.path.endsWith(".clj")) {
         child_process.exec("brepl balance " + event.input.path);
         ctx.ui.notify("Balanced parens in " + event.input.path);
